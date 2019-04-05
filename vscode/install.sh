830a14c03fc927765c44a0a27447b7521046abd6
Thanawat Petchsri <thanawat.petchsri@krungthai-axa.co.th>
2019-04-05 17:46:29 +0700

    Update extension

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,25 +1,37 @@
 echo "Installing extensions ..."
 
+code --install-extension 2gua.rainbow-brackets
+code --install-extension akamud.vscode-theme-onelight
+code --install-extension alefragnani.Bookmarks
 code --install-extension Arjun.swagger-viewer
+code --install-extension bierner.markdown-preview-github-styles
 code --install-extension chenxsan.vscode-standardjs
 code --install-extension cssho.vscode-svgviewer
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension deerawan.vscode-faker
+code --install-extension DotJoshJohnson.xml
+code --install-extension eamodio.gitlens
 code --install-extension EditorConfig.EditorConfig
 code --install-extension eg2.vscode-npm-script
 code --install-extension esbenp.prettier-vscode
 code --install-extension felixfbecker.php-intellisense
 code --install-extension file-icons.file-icons
+code --install-extension flowtype.flow-for-vscode
+code --install-extension jebbs.plantuml
 code --install-extension mauve.terraform
 code --install-extension mgmcdermott.vscode-language-babel
+code --install-extension miguelsolorio.min-theme
 code --install-extension mikestead.dotenv
+code --install-extension mp.vscode-oracle-format
 code --install-extension mrmlnc.vscode-apache
 code --install-extension ms-mssql.mssql
-code --install-extension ms-vscode.PowerShell
+code --install-extension ms-vscode.powershell
 code --install-extension PeterJausovec.vscode-docker
 code --install-extension pflannery.vscode-versionlens
+code --install-extension sibiraj-s.vscode-scss-formatter
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension wayou.vscode-todo-highlight
+code --install-extension xyz.plsql-language
 code --install-extension yzhang.markdown-all-in-one
 code --install-extension zhuangtongfa.Material-theme
 
